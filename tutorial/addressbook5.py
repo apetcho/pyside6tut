572546b30036c8dcf963f154e2bdcffdc3e7f0c9
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-22 20:58:22 +0100

    Implement FindDialog.__init__() method in version 5

--- tutorial/addressbook5.py
+++ tutorial/addressbook5.py
@@ -275,7 +275,24 @@ class AddressBook(QtWidgets.QWidget):
 class FindDialog(QtWidgets.QDialog):
 
     def __init__(self, parent=None):
-        pass
+        super(FindDialog, self).__init__(parent)
+
+        findLabel = QtWidgets.QLabel("Enter the name of a contact:")
+        self.lineEdit = QtWidgets.QLineEdit()
+
+        self.findButton = QtWidgets.QPushButton("&Find")
+        self.findText = ""
+
+        layout = QtWidgets.QHBoxLayout()
+        layout.addWidget(findLabel)
+        layout.addWidget(self.lineEdit)
+        layout.addWidget(self.findButton)
+
+        self.setLayout(layout)
+        self.setWindowTitle("Find a Contact")
+
+        self.findButton.clicked.connect(self.findClicked)
+        self.findButton.clicked.connect(self.accept)
 
     def findClicked(self):
         pass
