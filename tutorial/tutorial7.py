e86211eb3e945ec0eddd337dbb6008f11d36694e
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-23 00:28:34 +0100

    Implement MyWidget class in tutorial7

--- tutorial/tutorial7.py
+++ tutorial/tutorial7.py
@@ -4,6 +4,8 @@ from PySide6 import QtCore
 from PySide6 import QtGui
 from PySide6 import QtWidgets
 
+app = QtWidgets.QApplication(sys.argv)
+
 
 class LCDRange(QtWidgets.QWidget):
 
@@ -19,3 +21,15 @@ class LCDRange(QtWidgets.QWidget):
     @QtCore.Slot(int)
     def setValue(self, value):
         pass
+
+
+class MyWidget(QtWidgets.QWidget):
+
+    def __init__(self, parent=None):
+        super(MyWidget, self).__init__(parent)
+        # TODO
+
+
+widget = MyWidget()
+widget.show()
+sys.exit(app.exec())
