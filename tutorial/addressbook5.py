67934fe9c3fcd01eb52cdbdf7e894aa5448dc8f0
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-22 21:00:56 +0100

    Implement FindDialog.findClicked() method in version 5

--- tutorial/addressbook5.py
+++ tutorial/addressbook5.py
@@ -295,7 +295,15 @@ class FindDialog(QtWidgets.QDialog):
         self.findButton.clicked.connect(self.accept)
 
     def findClicked(self):
-        pass
+        text = self.lineEdit.text()
+        if not text:
+            QtWidgets.QMessageBox.information(self, "Empty Field",
+                "Please enter a name.")
+            return
+        else:
+            self.findText = text
+            self.lineEdit.clear()
+            self.hide()
 
     def getFindText(self):
         pass
