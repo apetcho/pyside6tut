2036cf7156d20b1c509e3bf9962326fe53b19af4
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-22 16:22:44 +0100

    Introduce QtWidgets.QWidget as base widget class

--- tutorial/addressbook1.py
+++ tutorial/addressbook1.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
+from ast import Add
 from PySide6 import QtCore
 from PySide6 import QtWidgets
 
 
-class AddressBook:
-    pass
+class AddressBook(QtWidgets.QWidget):
+
+    def __init__(self, parent=None):
+        super(AddressBook, self).__init__(parent)
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    addressbook = AddressBook()
+    addressbook.show()
+    sys.exit(app.exec())
