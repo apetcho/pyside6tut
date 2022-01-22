4635265067a2e9f63618a6246aa2026a7fdf3ada
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-22 16:28:26 +0100

    Introduce QLabel, QLineEdit, QTextEdit, and QGridLayout class

--- tutorial/addressbook1.py
+++ tutorial/addressbook1.py
@@ -9,6 +9,21 @@ class AddressBook(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super(AddressBook, self).__init__(parent)
 
+        nameLabel = QtWidgets.QLabel("Name:")
+        self.nameLine = QtWidgets.QLineEdit()
+
+        addressLabel = QtWidgets.QLabel("Address:")
+        self.addressText = QtWidgets.QTextEdit()
+
+        mainLayout = QtWidgets.QGridLayout()
+        mainLayout.addWidget(nameLabel, 0, 0)
+        mainLayout.addWidget(self.nameLine, 0, 1)
+        mainLayout.addWidget(addressLabel, 1, 0, QtCore.Qt.AlignTop)
+        mainLayout.addWidget(self.addressText, 1, 1)
+
+        self.setLayout(mainLayout)
+        self.setWindowTitle("Simple Address Book")
+
 
 if __name__ == "__main__":
     import sys
