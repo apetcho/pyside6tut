9dba8820c036f580fa8560678c6c735e6fc141ac
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-23 18:17:49 +0100

    Implement tutorial13 CannonField.cannonRect() method

--- tutorial/tutorial13.py
+++ tutorial/tutorial13.py
@@ -206,7 +206,9 @@ class CannonField(QtWidgets.QWidget):
         painter.restore()
 
     def cannonRect(self):
-        pass
+        result = QtCore.QRect(0, 0, 50, 50)
+        result.moveBottomLeft(self.rect().bottomLect())
+        return result
 
     def shotRect(self):
         pass
