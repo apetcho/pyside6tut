d814e7bbf554b67b49d60126ae38fb4d20be90d8
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-23 12:17:56 +0100

    Create tutorial12 LCDRange class skeleton

---
+++ tutorial/tutorial12.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import sys
+import math
+import random
+from PySide6 import QtCore
+from PySide6 import QtGui
+from PySide6 import QtWidgets
+
+
+class LCDRange(QtWidgets.QWidget):
+    valueChanged = QtCore.Signal(int)
+
+    def __init__(self, text=None, parent=None):
+        pass
+
+    def init(self):
+        pass
+
+    def value(self):
+        pass
+
+    @QtCore.Slot(int)
+    def setValue(self, value):
+        pass
+
+    def text(self):
+        pass
+
+    def setRange(self, vmin, vmax):
+        pass
+
+    def setText(self, text):
+        pass
+
