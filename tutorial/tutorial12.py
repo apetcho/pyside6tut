84e321eb4600750f08a713e4e182c7edb691cc89
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-23 16:53:36 +0100

    Implement tutorial12 CannonField.paintShot() method

--- tutorial/tutorial12.py
+++ tutorial/tutorial12.py
@@ -151,7 +151,9 @@ class CannonField(QtWidgets.QWidget):
         self.paintTarget(painter)
 
     def paintShot(self, painter: QtGui.QPainter):
-        pass
+        painter.setPen(QtCore.Qt.NoPen)
+        painter.setBrush(QtCore.Qt.black)
+        painter.drawRect(self.shotRect())
 
     def paintTarget(self, painter: QtGui.QPainter):
         pass
