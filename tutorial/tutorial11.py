7694093d2c22b7fdc12ec1df993ed3836bfeb58d
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-23 11:55:18 +0100

    Implement tutorial11 CannonField.shotRect() method

--- tutorial/tutorial11.py
+++ tutorial/tutorial11.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
 import math
+from unittest import result
 from PySide6 import QtCore
 from PySide6 import QtGui
 from PySide6 import QtWidgets
@@ -139,7 +140,20 @@ class CannonField(QtWidgets.QWidget):
         return result
 
     def shotRect(self):
-        pass
+        gravity = 4.0
+        time = self.timerCount / 40.0
+        velocity = self.shootForce
+        radians = self.shootAngle * math.pi / 180
+        velx = velocity * math.cos(radians)
+        vely = velocity * math.sin(radians)
+        x0 = (CannonField.barrelRect.right()+5)*math.cos(radians)
+        y0 = (CannonField.barrelRect.right()+5)*math.sin(radians)
+        x = x0 + velx * time
+        y = y0 + vely * time - 0.5*gravity*time*time
+
+        result = QtCore.QRect(0, 0, 6, 6)
+        result.moveCenter(QtCore.QPoint(round(x), self.height()-1-round(y)))
+        return result
 
 
 class MyWidget(QtWidgets.QWidget):
