1e31f05ea907f0b4779b0aa2bb30239153ed8616
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-23 02:53:30 +0100

    Implement LCDRange.setRange() tutorial9 skeleton

--- tutorial/tutorial9.py
+++ tutorial/tutorial9.py
@@ -38,7 +38,13 @@ class LCDRange(QtWidgets.QWidget):
         self.slider.setValue(value)
 
     def setRange(self, minVal, maxVal):
-        pass
+        if minVal < 0 or maxVal > 99 or minVal > maxVal:
+            QtCore.qWarning(
+                f"LCDRange.setRange({minVal}, {maxVal})\n"
+                "\tRange must be 0..9\n"
+                "\tand minValue must not be greater than maxValue")
+            return
+        self.slider.setRange(minVal, maxVal)
 
 
 class CannonField(QtWidgets.QWidget):
