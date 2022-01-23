a30332e13b234fc7af7e1d2f122096ca7dd805d2
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-23 16:57:54 +0100

    Implement tutorial12 CannonField.paintCannon() method

--- tutorial/tutorial12.py
+++ tutorial/tutorial12.py
@@ -164,7 +164,14 @@ class CannonField(QtWidgets.QWidget):
 
 
     def paintCannon(self, painter: QtGui.QPainter):
-        pass
+        painter.setPen(QtCore.Qt.NoPen)
+        painter.setBrush(QtCore.Qt.blue)
+        painter.save()
+        painter.translate(0, self.height())
+        painter.drawPie(QtCore.QRect(-35, -35, 70, 70), 0, 90*16)
+        painter.rotate(-self.currentAngle)
+        painter.drawRect(CannonField.barrelRect)
+        painter.restore()
 
     def cannonRect(self):
         pass
