ded0f9c3322efedc5e9cbc9748707982e7504773
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-23 02:48:42 +0100

    Implement LCDRange.__init__() tutorial9 skeleton

--- tutorial/tutorial9.py
+++ tutorial/tutorial9.py
@@ -12,7 +12,23 @@ class LCDRange(QtWidgets.QWidget):
 
     def __init__(self, parent=None):
         super(LCDRange, self).__init__(parent)
-        # TODO
+
+        lcd = QtWidgets.QLCDNumber(2)
+        self.slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        self.slider.setRange(0, 99)
+        self.slider.setValue(0)
+
+        self.connect(self.slider, QtCore.SIGNAL("valueChanged(int)"),
+            lcd, QtCore.SLOT("display(int)"))
+        self.connect(self.slider, QtCore.SIGNAL("valueChanged(int)"),
+            self, QtCore.SIGNAL("valueChanged(int)"))
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(lcd)
+        layout.addWidget(self.slider)
+        self.setLayout(layout)
+
+        self.setFocusProxy(self.slider)
 
     def value(self):
         pass
