e22c596f321f7a2c115376b3fc7eda76ab2fce5d
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-23 17:45:19 +0100

    Implement tutorial13 LCDRange.value() method

--- tutorial/tutorial13.py
+++ tutorial/tutorial13.py
@@ -42,7 +42,7 @@ class LCDRange(QtWidgets.QWidget):
         self.setFocusProxy(self.slider)
 
     def value(self):
-        pass
+        return self.slider.value()
 
     @QtCore.Slot(int)
     def setValue(self, value):
