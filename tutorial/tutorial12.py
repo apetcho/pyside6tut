a5cc4431dbd9ee73585b9664558cee8446af4263
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-23 12:24:07 +0100

    Create tutorial12 CannonField class skeleton

--- tutorial/tutorial12.py
+++ tutorial/tutorial12.py
@@ -32,3 +32,62 @@ class LCDRange(QtWidgets.QWidget):
     def setText(self, text):
         pass
 
+
+class CannonField(QtWidgets.QWidget):
+
+    angleChanged = QtCore.Signal(int)
+    forceChanged = QtCore.Signal(int)
+
+    def __init__(self, parent=None):
+        pass
+
+    def angle(self):
+        pass
+
+    @QtCore.Slot(int)
+    def setAngle(self, angle):
+        pass
+
+    def force(self):
+        pass
+
+    @QtCore.Slot(int)
+    def setForce(self, force):
+        pass
+
+    @QtCore.Slot()
+    def shoot(self):
+        pass
+
+    firstTime = True
+
+    def newTarget(self):
+        pass
+
+    @QtCore.Slot()
+    def moveShot(self):
+        pass
+
+    def paintEvent(self, event: QtCore.QEvent):
+        pass
+
+    def paintShot(self, painter: QtGui.QPainter):
+        pass
+
+    def paintTarget(self, painter: QtGui.QPainter):
+        pass
+
+    barrelRect = QtCore.QRect(33, -4, 15, 8)
+
+
+    def paintCannon(self, painter: QtGui.QPainter):
+        pass
+
+    def cannonRect(self):
+        pass
+
+    def shotRect(self):
+        pass
+
+    def targetRect(self):
+        pass
