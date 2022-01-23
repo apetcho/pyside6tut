f89b610f73d598669de1547f4db8e6808c312236
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-23 19:11:56 +0100

    Create tutorial14 CannonField class skeleton

--- tutorial/tutorial14.py
+++ tutorial/tutorial14.py
@@ -32,3 +32,98 @@ class LCDRange(QtWidgets.QWidget):
 
     def setText(self, text):
         pass
+
+
+class CannonField(QtWidgets.QWidget):
+
+    angleChanged = QtCore.Signal(int)
+    forceChanged = QtCore.Signal(int)
+    hit = QtCore.Signal()
+    missed = QtCore.Signal()
+    canShoot = QtCore.Signal(bool)
+
+    def __init__(self, parent=None):
+        pass
+
+    def angle(self):
+        pass
+
+    @QtCore.Slot(int)
+    def setAngle(self, angle):
+        pass
+
+    def force(self):
+        pass
+
+    @QtCore.Slot(int)
+    def setForce(self, force):
+        pass
+
+    @QtCore.Slot()
+    def shoot(self):
+        pass
+
+    firstTime = True
+
+    def newTarget(self):
+        pass
+
+    def setGameOver(self):
+        pass
+
+    def restartGame(self):
+        pass
+
+    @QtCore.Slot()
+    def moveShot(self):
+        pass
+
+    def mousePressEvent(self, event: QtCore.QEvent):
+        pass
+
+    def mouseMoveEvent(self, event: QtCore.QEvent):
+        pass
+
+    def mouseReleaseEvent(self, event: QtCore.QEvent):
+        pass
+
+    def paintEvent(self, event: QtCore.QEvent):
+        pass
+
+    def paintShot(self, painter: QtGui.QPainter):
+        pass
+
+    def painterTarget(self, painter: QtGui.QPainter):
+        pass
+
+    def paintBarrier(self, painter: QtGui.QPainter):
+        pass
+
+    barrelRect = QtCore.QRect(33, -4, 15, 8)
+
+    def paintCannon(self, painter: QtGui.QPainter):
+        pass
+
+    def cannonRect(self):
+        pass
+
+    def shotRect(self):
+        pass
+
+    def targetRect(self):
+        pass
+
+    def barrierRect(self):
+        pass
+
+    def barrelHit(self, pos):
+        pass
+
+    def gameOver(self):
+        pass
+
+    def isShooting(self):
+        pass
+
+    def sizeHint(self):
+        pass
