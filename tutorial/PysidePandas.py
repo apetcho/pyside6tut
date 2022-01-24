cd0955733a7d966f684cecacc93e5c74c0bcf798
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-24 13:46:31 +0100

    Create PandasModel class skeleton in PysidePandas.py project

--- tutorial/PysidePandas.py
+++ tutorial/PysidePandas.py
@@ -4,3 +4,44 @@ import pandas as pd
 from PySide6.QtWidgets import QTableView, QApplication
 from PySide6.QtCore import QAbstractTableModel, Qt, QModelIndex
 
+from typing import Any
+
+
+class PandasModel(QAbstractTableModel):
+    """A model to interface a Qt view with pandas dataframe."""
+
+    def __init__(self, dframe: pd.DataFrame, parent=None):
+        # super(PandasModel, self).__init__(parent)
+        pass
+
+    def rowCount(self, parent=QModelIndex()) -> int:
+        """Override method from QAbstractTableModel.
+        
+        Return row count of the pandas DataFrame
+        """
+        pass
+
+    def columnCount(self, parent=QModelIndex()) -> int:
+        """Override method from QAbstractTableModel.
+
+        Return row count of the pandas DataFrame
+        """
+        pass
+
+    def data(self, index: QModelIndex, role=Qt.ItemDataRole):
+        """Override metho from QAbstractTableModel.
+        
+        Retur data cell from the pandas DataFrame
+        """
+        pass
+
+    def headerData(
+        self, section: int, orientation: Qt.Orientation,
+        role: Qt.ItemDataRole
+    ) -> Any:
+        """Override method from QAbstractTableModel.
+        
+        Return dframe index as vertical header data and columns as horizontal
+        header data.
+        """
+        pass
