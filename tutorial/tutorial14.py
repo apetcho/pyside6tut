bdda8ff297aa33d2cd665385426776a79f8f8194
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-24 16:22:20 +0100

    Implement tutorial14 CannonField.newTarget() method

--- tutorial/tutorial14.py
+++ tutorial/tutorial14.py
@@ -135,7 +135,16 @@ class CannonField(QtWidgets.QWidget):
     firstTime = True
 
     def newTarget(self):
-        pass
+        if CannonField.firstTime:
+            CannonField.firstTime = False
+            midnight = QtCore.QTime(0, 0, 0)
+            random.seed(midnight.secsTo(QtCore.QTime.currentTime()))
+
+        self.target = QtCore.QPoint(
+            200 + random.randint(0, 190-1),
+             10 + random.randint(0, 255-1)
+        )
+        self.update()
 
     def setGameOver(self):
         pass
