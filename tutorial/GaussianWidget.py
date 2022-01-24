832410982c42225f8939b9bdf89220abf7009bd6
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-24 11:47:16 +0100

    Implement PlotWidget class

--- tutorial/GaussianWidget.py
+++ tutorial/GaussianWidget.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy.stats import norm
 from matplotlib import pyplot as plt
 from matplotlib.figure import Figure
-from matplotlib.backends.backend_qt5agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import FigureCanvasQT as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT
 from PySide6.QtCore import Qt, Slot
 from PySide6.QtWidgets import (
@@ -15,8 +15,45 @@ from PySide6.QtWidgets import (
 class PlotWidget(QWidget):
 
     def __init__(self, parent=None):
-        pass
+        super(PlotWidget, self).__init__(parent)
+
+        # Create widgets
+        self.view = FigureCanvas(Figure(figsize=(5, 3)))
+        self.axes = self.view.figure.suplots()
+        self.toolbar = NavigationToolbar2QT(self.view, self)
+        self.mu_input = QDoubleSpinBox()
+        self.std_input = QDoubleSpinBox()
+        self.mu_input.setPrefix("µ: ")
+        self.std_input.setPrefix("∑: ")
+        self.std_input.setValue(10)
+
+        # Create layout
+        input_layout = QHBoxLayout()
+        input_layout.addWidget(self.mu_input)
+        input_layout.addWidget(self.std_input)
+        vlayout = QVBoxLayout()
+        vlayout.addWidget(self.toolbar)
+        vlayout.addWidget(self.view)
+        vlayout.addLayout(input_layout)
+        self.setLayout(vlayout)
+
+        # Connect inputs with on_change method
+        self.mu_input.valueChanged.connect(self.on_change)
+        self.std_input.valueChanged.connect(self.on_change)
+
+        self.on_change()
 
     @Slot()
     def on_change(self):
-        pass
+        """Update the plot with the current input values."""
+        mu = self.mu_input.value()
+        std = self.std_input.value()
+        x = np.linspace(-100, 100)
+        y = norm.pdf(x, mu, std)
+
+        self.axes.clear()
+        self.axes.plot(x, y)
+        self.view.draw()
+
+
+
